02bd95a1c75eb8434f6a54c9d9376929b57649ea
sasasa3066 <d0584776@mail.fcu.edu.tw>
2019-11-16 03:23:33 +0800

    所有功能完成，還有一些小問題沒有修正

--- src/com/company/Chat.java
+++ src/com/company/Chat.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.*;
 import java.net.*;
 
 import static java.awt.event.KeyEvent.VK_ENTER;
@@ -21,19 +22,21 @@ public class Chat extends JFrame{ //extends JFrame是因為大量使用JFrame類
     private JPanel southPanel;
     private JPanel centerPanel;
     private DatagramSocket socket = null;
-
+    private BufferedWriter writer;
 
     public Chat() {
         init();//因為如果全部的東西都寫在構造方法中會很擁擠，所以分開寫
         event();
         Receive receive = new Receive();
         receive.start();
+
     }
 
 
     @Override
     protected void finalize() throws Throwable {
         socket.close();
+        writer.close();
     }
 
     public void init(){
@@ -42,7 +45,8 @@ public class Chat extends JFrame{ //extends JFrame是因為大量使用JFrame類
         getContentPane().add(panel1);
         try{
             socket = new DatagramSocket();
-        }catch (SocketException e){
+            writer = new BufferedWriter(new FileWriter("record.txt",true));//true是追加功能，而不是清空寫入
+        }catch (Exception e){
             System.out.println(e);
         }
         setVisible(true);
@@ -57,25 +61,104 @@ public class Chat extends JFrame{ //extends JFrame是因為大量使用JFrame類
                     System.out.println(e);
                 }
             }
-
-
         });
+
+        /**
+         * 快捷鍵功能(Ctrl + Enter)，不用每次都用滑鼠點擊button才能寄出訊息
+         */
+        messageText.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if(e.getKeyCode() == VK_ENTER && e.isControlDown()){ //Ctrl + Enter同時成立
+                    try{
+                        send();
+                    }catch(Exception e1){
+                        System.out.println(e1);
+                    }
+                }
+            }
+        });
+
         recordButton.addActionListener(new ActionListener(){
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                try {
+                    record();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
 
+        cleanButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                messageTextArea.setText("");
+            }
+        });
+        quakeButton.addActionListener(new ActionListener() { //震動對方
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    /**
+                     * //選擇-1是為了有識別性，又不會再打字的過程中被打出來ex:'a'就有可能在聊天內容中有a
+                     * 所以選-1不可能用打字的方式可以打出-1因為那是"-1"不是字節的-1
+                     */
+                    send(new byte[]{-1},textField1.getText());
+                }catch (Exception e1){
+
+                }
             }
         });
     }
 
-    public void send() throws SocketException, UnknownHostException {
+    private void send(byte[] arr,String ip) throws IOException{ //將此功能從send()抽取出來，做成function重複利用
+        DatagramPacket packet =
+                new DatagramPacket(arr,arr.length, InetAddress.getByName(ip),7000);
+        socket.send(packet);
+    }
+
+    private void send() throws IOException {
+        String ip = textField1.getText();
+        ip = (ip.trim().length() == 0) ? "255.255.255.255" : ip; //優化:如果IP為空的，那就是對所有人說用廣播位置 trim去頭尾的空白
         String message = messageText.getText();
         System.out.println(message);
-        String ip = textField1.getText();
-        DatagramPacket packet = new DatagramPacket(message.getBytes(),message.getBytes().length, InetAddress.getByName(ip),7000);
-        messageTextArea.append("\nTim:" + message);//這邊得用append 不然用setText會覆蓋掉之前的內容
+        send(message.getBytes(),ip);
+        message = "I say:" + message + "\n";
+        messageTextArea.append(message);//這邊得用append 不然用setText會覆蓋掉之前的內容
+        writer.write(message);
         messageText.setText("");  //訊息發送完後將輸入區的內容給清空
+
+    }
+
+    private void record() throws IOException{
+        writer.flush(); //如果不刷新有些資料可能會在緩衝區中，在沒有調用close方法前不會寫入record.txt中，除非緩衝區滿了才會送出
+        FileInputStream fis = new FileInputStream("record.txt");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int len;
+        byte[] arr = new byte[8192];
+        while((len = fis.read(arr)) != -1){
+            baos.write(arr,0,len);
+        }
+        String s = baos.toString();
+        messageTextArea.setText(s); //在此不能用append，如果點兩次record就會出現兩次紀錄所以得用setText
+    }
+
+    private void quake() throws InterruptedException { //震動就是一直改變起始座標就好了
+        int x = this.getLocation().x;
+        int y = this.getLocation().y;
+        for(int i = 2 ; i<5 ; i++){
+            this.setLocation(x += 20, y += 20);
+            Thread.sleep(30);
+            this.setLocation(x += 20, y -= 20);
+            Thread.sleep(30);
+            this.setLocation(x -= 20, y += 20);
+            Thread.sleep(30);
+            this.setLocation(x -= 20, y -= 20);
+            Thread.sleep(30);
+        }
+
 
     }
 
@@ -88,9 +171,16 @@ public class Chat extends JFrame{ //extends JFrame是因為大量使用JFrame類
                 while(true){
                     socketc.receive(packet);
                     byte[] arr = packet.getData();
+                    if(arr[0] == -1 && packet.getLength() == 1){ //當接收到震動，就執行到震動
+                        quake();
+                        System.out.println("quake!!!!!!");
+                        continue;
+                    }
                     String s = new String(arr,0,packet.getLength());
                     s = new String(s.getBytes());
-                    messageTextArea.append("\n" + packet.getAddress() + ":" + s);
+                    s = packet.getAddress() + ":" + s + "\n";
+                    messageTextArea.append(s);
+                    writer.write(s);
                     System.out.println(s);
                 }
             }catch (Exception e1){
